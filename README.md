fbd4f21fe3e9e85708736436de0fc3fcbc1a302b
Darren Hewett <darrenhewett@users.noreply.github.com>
2016-09-07 08:58:37 +0100

    Switched to server JSON file for data

--- README.md
+++ README.md
@@ -2,37 +2,6 @@
 
 
 
-## Install the Polymer-CLI
+## Proof of concept for large list of data
 
-First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) installed. Then run `polymer serve` to serve your application locally.
-
-## Viewing Your Application
-
-```
-$ polymer serve
-```
-
-## Building Your Application
-
-```
-$ polymer build
-```
-
-This will create a `build/` folder with `bundled/` and `unbundled/` sub-folders
-containing a bundled (Vulcanized) and unbundled builds, both run through HTML,
-CSS, and JS optimizers.
-
-You can serve the built versions by giving `polymer serve` a folder to serve
-from:
-
-```
-$ polymer serve build/bundled
-```
-
-## Running Tests
-
-```
-$ polymer test
-```
-
-Your application is already set up to be tested via [web-component-tester](https://github.com/Polymer/web-component-tester). Run `polymer test` to run your application's test suite locally.
+Data is generated and stored in a JSON file on the server, loaded into an /<iron-list/> element and filtered.
---
+++ gen-data.js
@@ -0,0 +1,45 @@
+// Generate random database of genealogy data
+var generator = new Promise(function(resolve, reject) {
+  var tempArray= [];
+  var birth, death;
+
+  function genNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function genName() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for ( var i = 0; i < 6; i++ )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
+  for ( var a = 0; a < 1e6; a++ ) {
+    birth = genNumber(1200, 2000).toFixed(0);
+    death = (parseFloat(birth) + genNumber(0, 100)).toFixed(0);
+    tempArray.push({ 
+      firstName: genName(), lastName: genName(),
+      birth: birth, death: death
+    });
+  }
+  resolve(tempArray);
+});
+
+// use promise to generate data async
+generator.then(function(result) {
+  var fs = require('fs');
+  var jsonData = { data: result };
+
+  var stringData = JSON.stringify(jsonData);
+
+  fs.writeFile('data.json', stringData,
+  function (err) {
+    if (err) return console.log(err);
+    console.log('File write')
+  });
+}, function(err) {
+  console.log(err);
+});
+
